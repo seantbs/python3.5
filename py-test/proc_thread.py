5c213aecc09bf525f37878702f93338727700860
seantbs <seantbs@163.com>
2018-12-29 00:20:19 +0800

    20181229-0020

--- py-test/proc_thread.py
+++ py-test/proc_thread.py
@@ -1,74 +1,81 @@
 #!/usr/bin/python3
 
-from multiprocessing import Process,Pool,Queue
-import os,sys,threading,time,random
+from multiprocessing import Process,Pool,Value
+import os,sys,io,threading,time,random,queue,logging
 
 print("start time:",time.ctime())
 st=time.time()
 
-def ty(s,e):
-	if s != e:
-		for i in range(s,e):
-			i=i+1
-			yield i
-	else:
-		print("yield is done.")
-def q_put(g):
-	if ports != porte:
-		while not portq.full():
+def g(s,e):
+	pts.value=pts.value+1
+	for i in range(s,e):
+		#print(threading.current_thread().name,"of g=",i)
+		yield i
+
+def pq_put():
+	global pq
+	for i in range(pts.value,pte.value):
+		if pts.value == i and pts.value < pte.value:
+			pg=g(pts.value,pts.value+1)
 			try:
-				portq.put(next(g))
-				print("there are still",porte-ports,"to queue in",os.getpid())
+				pq.put(next(pg))
 			except:
 				break
-	else:
-		print("there is nothing to queue.")
+		elif pts.value != i:
+		#print("pq_put of",threading.current_thread().name,"=",pts.value)
 
-def ptest(pv,ths,test):
-	print(os.getpid(),"is running.code =",pv)
-	test=test+test
+def res(ramf):
+	print(ramf)
+	with open('./test.txt','w') as f:
+		f.write(ramf)
+
+def pfunc(pcode,ths):
+	print(os.getpid(),"is running.code =",pcode)
 	thpool=[]
 	for i in range(ths):
-		t = threading.Thread(target=ttest,args=(),name="threadid-"+str(os.getpid())+r"/"+str(i))
+		t = threading.Thread(target=tfunc,args=(),name="threadid-"+str(os.getpid())+r"/"+str(i))
 		thpool.append(t)
-		#print(threading.current_thread().name,"of",os.getpid(),"is appended.")
-	print(test)
 	for a in thpool:
 		a.start()
-		#print(threading.current_thread().name,"of",os.getppid(),"is starting.")
 	for b in thpool:
 		b.join()
-		#print("thread",threading.current_thread().name,"of",os.getpid(),"is ended.")
 
-def ttest():
-	#print(threading.current_thread().name,"start time",time.time())
-	while not portq.empty():
-		print(threading.current_thread().name,"of",os.getpid(),"is running.code =",portq.get())
+def tfunc():
+	while not pq.empty():
+		ramf.write(threading.current_thread().name+" of is running.code =\t"+str(pq.get())+"\n")
+		#time.sleep(0.01)
+		#print(threading.current_thread().name,"is running.code =\t",pq.get())
 		r=random.randint(1,2)
-		#print("user time:",r)
 		time.sleep(r)
+	pq_put()
+	res(ramf.getvalue())
 
 if __name__=='__main__':
-	pv=123
-	ps=2
-	ths=2
-	ports=0
-	porte=20
-	test=100
-	portq=Queue(ps*ths)
-	y=ty(ports,porte)
-	q_put(y)
+	pcode=123456
+	procs=2
+	ths=10
+	ports=1
+	porte=101
 
+	ramf=io.StringIO()
+	os.remove('./test.txt')
+	os.mknod('./test.txt')
+	pts=Value('i',ports)
+	pte=Value('i',porte)
+	print("pts.value=%i,pte.value=%i"%(pts.value,pte.value))
+	pq=queue.Queue(ths)
+	
 	print("="*60)
 	print('Parent process %s.' % os.getppid())
 	print("="*60)
-	p=Pool(ps)
-	for i in range(ps):
-		p.apply_async(ptest,args=(pv,ths,test,))
+	p=Pool(procs)
+	for i in range(procs):
+		p.apply_async(pfunc,args=(pcode,ths,))
 	p.close()
 	p.join()
-
-	print("main process",os.getppid(),"(child process:",os.getpid(),") end.")
+	
 	print("real time:",time.time()-st)
+	print("main process",os.getppid(),"(child process:",os.getpid(),") end.")
+
 	print("end time:",time.ctime())
 
