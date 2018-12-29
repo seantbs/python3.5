d9adf12c615471f5ea2977777a9714581b19caa5
seasntbs <seantbs@163.com>
2018-12-29 17:11:35 +0800

    Update proc_thread.py

--- py-test/proc_thread.py
+++ py-test/proc_thread.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
 
-from multiprocessing import Process,Pool,Value
+from multiprocessing import Process,Pool,Value,Array
 import os,sys,io,threading,time,random,queue,logging
 
 print("start time:",time.ctime())
 st=time.time()
 
+def check(s):
+	print("check",threading.current_thread().name,"s =",s)
+	for i in chk:
+		print("chk[%i]=%i"%(i,i))
+	chk.append(s)
+	for i in chk:
+		print("append then chk[%i]=%i"%(i,i))
+	if chk[0] == chk[1]:
+		print(threading.current_thread().name,"pts =",chk[0])
+		pq_put()
+	else:
+		return 1
 def g(s,e):
-	pts.value=pts.value+1
-	for i in range(s,e):
-		#print(threading.current_thread().name,"of g=",i)
-		yield i
+	if check(s):
+		pts.value=pts.value+1
+		for i in range(s,e):
+			#print(threading.current_thread().name,"of g=",i)
+			yield i
 
 def pq_put():
 	global pq
 	for i in range(pts.value,pte.value):
-		if pts.value == i and pts.value < pte.value:
-			pg=g(pts.value,pts.value+1)
-			try:
-				pq.put(next(pg))
-			except:
-				break
-		elif pts.value != i:
-		#print("pq_put of",threading.current_thread().name,"=",pts.value)
+		pg=g(i,i+1)
+		try:
+			pq.put(next(pg))
+		except:
+			break
+		print("pq_put of",threading.current_thread().name,"=",pts.value)
 
 def res(ramf):
-	print(ramf)
-	with open('./test.txt','w') as f:
+	with open('./result.txt','a') as f:
 		f.write(ramf)
 
 def pfunc(pcode,ths):
@@ -41,10 +51,10 @@ def pfunc(pcode,ths):
 		b.join()
 
 def tfunc():
+	print("tfunc",threading.current_thread().name,"pts =",pts.value)
 	while not pq.empty():
 		ramf.write(threading.current_thread().name+" of is running.code =\t"+str(pq.get())+"\n")
-		#time.sleep(0.01)
-		#print(threading.current_thread().name,"is running.code =\t",pq.get())
+		print(threading.current_thread().name,"is running.code =\t",pq.get())
 		r=random.randint(1,2)
 		time.sleep(r)
 	pq_put()
@@ -58,10 +68,15 @@ if __name__=='__main__':
 	porte=101
 
 	ramf=io.StringIO()
-	os.remove('./test.txt')
-	os.mknod('./test.txt')
+	try:
+		os.remove('./result.txt')
+	except:
+		pass
+	os.mknod('./result.txt')
+
 	pts=Value('i',ports)
 	pte=Value('i',porte)
+	chk=Array('i',[0,0])
 	print("pts.value=%i,pte.value=%i"%(pts.value,pte.value))
 	pq=queue.Queue(ths)
 	
@@ -78,4 +93,3 @@ if __name__=='__main__':
 	print("main process",os.getppid(),"(child process:",os.getpid(),") end.")
 
 	print("end time:",time.ctime())
-
