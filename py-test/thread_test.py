969a7489d55f65e3fb4ea4ad39125d955a0e54d1
seasntbs <seantbs@163.com>
2018-12-29 14:54:21 +0800

    Update thread_test.py

--- py-test/thread_test.py
+++ py-test/thread_test.py
@@ -1,76 +1,52 @@
-﻿#!/usr/bin/python3
+#!/usr/bin/python3
 
-import sys,time,threading,argparse
+import os,io,sys,queue,threading,time,random
 
-parser=argparse.ArgumentParser(description='input threads number value that you need .')
-parser.add_argument("-v", "--version",action='version', version='%(prog)s 1.0')
-parser.add_argument('-ths',type=int,nargs='?',default=4,help='set threads number value.the default value is 4 threads.')
-parser.parse_args()
-args=parser.parse_args()
+stime=time.time()
 
-print("start test:",time.ctime())
+def y(s,e):
+	global a
+	#print("y func a =",a)
+	if s == a:
+		a=a+1
+		for i in range(s,e):
+			yield i
+	else:
+		pg_put()
+def pq_put():
+	global pq,a,b
+	for i in range(a,b):
+		if a == i and a < b:
+			g=y(i,i+1)
+			try:
+				 pq.put(next(g))
+			except:
+				break
+		elif a != i:
+			continue
+		#print("pq func a =",a)
 
-#create generator
-def prange_test(a):
-        i=0
-        while i<a:
-                i=i+1
-                yield i
+def pq_get():
+	global pq,a
+	#print("pq_get func a =",a)
+	while not pq.empty():
+		print(threading.current_thread().name,"is running code =\t",pq.get())
+		time.sleep(random.randint(1,2))
+	pq_put()
 
-#create test func include lock and event
-def pg(y):
-	global p
-	while p!=0:
-		event.set()
-		try:
-			print(threading.current_thread().name,"handle port ",next(y))
-			time.sleep(1)
-		except:
-			break
-		plock.acquire()
-		p=p-1
-		print("new p value =",p)
-		plock.release()
-		event.wait()
+if __name__=='__main__':
+	a=1
+	b=10001
+	ths=100
+	pq=queue.Queue(ths)
+	
+	thp=[]
+	for i in range(ths):
+		t=threading.Thread(target=pq_get,args=(),name="threadid-"+str(i))
+		thp.append(t)
+	for c in thp:
+		c.start()
+	for d in thp:
+		d.join()
 
-
-#main pg
-#define generator
-p=20
-test=prange_test(p)
-
-#check ths args.ths value
-def check_ths(t,p):
-	if t > (p*0.01) or t > 1024:
-		t=int(p*0.01)+1
-		print(t)
-		print("just %d ports will be scaned.but you want to set threads %d !? are you sure? seriously? maybe %d threads will be enough." % (p,args.ths,t))
-	elif p < 4:
-		t=p
-		print(t)
-	return t
-
-#ths=check_ths(args.ths,p)
-ths=args.ths
-
-#create event object and threads pool
-thpool=[]
-event=threading.Event()
-plock=threading.Lock()
-for i in range(ths):
-	tname="threadID"+str(i+1)
-	th = threading.Thread(target=pg,args=(test,),name=tname)
-	thpool.append(th)
-	#print(thpool)
-
-#start threads
-for th in thpool:
-	th.start()
-
-#wait and end threads
-for th in thpool:
-	th.join()
-	print('%d threads are running...' % threading.active_count())
-
-print('thread %s ended.' % threading.current_thread().name)
-print('end threads time:',time.ctime())
+	print("real time:",time.time()-stime)
