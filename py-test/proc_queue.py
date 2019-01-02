b89b59ed3d444a71f51e9162ececf4ff7f23af12
seasntbs <seantbs@163.com>
2019-01-02 17:16:26 +0800

    Update proc_queue.py

--- py-test/proc_queue.py
+++ py-test/proc_queue.py
@@ -2,71 +2,83 @@
 
 from multiprocessing import Process,Pool,Queue,Value,Array
 import os,threading,queue,time,random
+print("start running:",time.ctime())
+
+def pw_y(a):
+  while not pw.full():
+    for i in range(a):
+      i=i*2
+      pw.put(i)
+  return
 
 def y(a,b):
   for i in range(a,b):
-    i=i+1
     yield i
 
-def check():
-  print("proc weight code:",w)
-  try:
-      q.put_nowait(w)
-      return pa
-  except:
-      while q.empty():
-          q.put_nowait(w)
-      return pa
+def check(w):
+  if pa.value >= pb:
+    return 0
+  else:
+    while pq.empty():
+      pq.put(w)
+      #print("put proc id=",os.getpid(),"weight code:",w)
+      return 1
+    tfunc(w)
 
-
-def pq_put():
-  a=check()
-  if check():
-    pg=y(a,pe)
-    while not pq.full():
-      pq.put_nowait(next(pg))
-      pa=pa+1
-    q.get_nowait()
+def tq_put(w):
+  if check(w) and pa.value < pb:
+    pg=y(pa.value,pb)
+    while not tq.full():
+      try:
+        tq.put(next(pg))
+        pa.value+=1
+      except:
+        return
+    pq.get()
+    #print("get proc id=",os.getpid(),"weight code:",pq.get())
+    tfunc(w)
+  else:
+    return
 
 def pfunc(pv,ths):
-  w=pw.pop()
+  w=pw.get()
   print("pfunc that pid=",os.getpid(),"is runcode=",pv,"wcode=",w)
   thpool=[]
   for i in range(ths):
-      t=threading.Thread(target=tfunc,args=(),name="tid"+str(os.getpid())+r"/"+str(i))
+      t=threading.Thread(target=tfunc,args=(w,),name="tid"+str(os.getpid())+r"/"+str(i))
       thpool.append(t)
   for a in thpool:
       a.start()
   for b in thpool:
       b.join()
 
-def tfunc():
-  while not pq.empty():
-      print("tfunc that",threading.current_thread().name,"is running code=",pq.get())
+def tfunc(w):
+  #print(threading.current_thread().name,"tq empty is",tq.empty())
+  while not tq.empty():
+    try:
+      print("tfunc that",threading.current_thread().name,"is running code=\t",tq.get_nowait())
       time.sleep(random.randint(1,2))
-  pq_put()
+    except:
+      return
+  tq_put(w)
 
 if __name__=='__main__':
   tstart=time.time()
   pv=123456
-  ps=2
-  ths=4
-  pw=[1,2]
-  pa=Value("i",1)
-  pb=Value("i",10)
-    
-  pq=queue.Queue(ths)
-  q=Queue(1)
-  p1=Process(target=pfunc,args=(pv,ths,),name="pid"+str(os.getpid()))
-  p2=Process(target=pfunc,args=(pv,ths,),name="pid"+str(os.getpid()))
-  p1.start()
-  p2.start()
-  p1.join()
-  p2.join()
-  #p=Pool(ps)
-  #for i in range(ps):
-  #  p=multiprocessing.apply_async(pfunc,args=(pv,ths,pq,ports,porte,))
-  #p.close()
-  #p.join()
+  procs=os.cpu_count()
+  ths=300
+  tq=queue.Queue(ths)
+
+  pa=Value('i',1)
+  pb=100001
+  pw=Queue(procs)
+  pw_y(procs)
+  pq=Queue(1)
+
+  p=Pool(procs)
+  for i in range(procs):
+    p.apply_async(pfunc,args=(pv,ths,))
+  p.close()
+  p.join()
   tend=time.time()
   print("real time:",tend-tstart)
