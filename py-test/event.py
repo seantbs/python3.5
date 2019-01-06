ccb7d9eb5383e34c96d579f93f27d7800bc6594e
seantbs <seantbs@163.com>
2019-01-06 18:17:43 +0800

    Update event.py

--- py-test/event.py
+++ py-test/event.py
@@ -3,47 +3,65 @@
 from multiprocessing import Event,Queue,Pool,Process,Value
 import os,time,random,threading,queue
 
+def eq_y(a):
+	for i in range(a):
+		if a >= wqs:
+			i=c.value-wqs
+			c.value=i
+			yield i
+		if c.value < wqs and c.value >0:
+			yield c.value
+
 def wq_y(a,b):
 	for i in range(a,b):
-		i*=10
+		i+=1
 		yield i
 		
 
 def eq_put():
 	print('[eq_put]prepare value...')
-	if c.value == 0:
+	if c.value < 0:
 		return 0
-	elif c.value > 0:
-		print('[eq_put]eq put value...')
-		eq.put(c.value)
-		c.value-=1
+	elif c.value >= 0:
+		g=eq_y(c.value)
+		while not eq.full():
+			try:
+				print('[eq_put]eq put value...')
+				eq.put(next(g))
+			except:
+				break
 		return 1
-	efunc()
 
 def wq_put():
-	global count,i
-	if not eq.empty():
+	global i
+	if not eq.empty() and eq_get == 0:
 		i=eq.get()
-		print('[wq_put]eq get value :',i)
+		eq_get=1
+		print('[wq_put]',os.getpid(),'eq get value :',i)
 		ee.set()
-		print('[wq_put]ee is_set :',ee.is_set())
-	if i >= 1:
-		g=wq_y(i,count)
+		#print('[wq_put]ee is_set :',ee.is_set())
+	if i > 0:
+		g=wq_y(i-10,i)
 		while not wq.full():
-			wq.put(next(g))
+			try:
+				wq.put(next(g))
+			except:
+				break
 		tfunc()
-
+	elif i == 0:
+		#print('[wq_put]',threading.current_thread().name,'wq_put is done...')
+		return
 def pefunc():
 	print(os.getpid(),'pefunc is running...')
 	c_e_th()
 
 def pwfunc():
 	print(os.getpid(),'pwfunc is running...')
-	c_w_th()
+	c_w_th(ths)
 
 def tfunc():
 	while not wq.empty():
-		print(threading.current_thread().name,'is code =',wq.get())
+		print('[tfunc]',threading.current_thread().name,'is running code =',wq.get())
 		r=random.randint(1,2)
 		time.sleep(r)
 	print('[tfunc]now wq is empty,wait wq put...')
@@ -55,11 +73,12 @@ def efunc():
 	while True:
 		if eq_put():
 			we.set()
-			print('[efunc]ee flag is',ee.is_set())
+			#print('[efunc]ee flag is',ee.is_set())
 			ee.wait()
 			eq_put()
 		elif not eq_put():
-			print("c value = 1,so pefunc done")
+			ee.clear()
+			print("c value = 0,so pefunc done")
 			break
 
 def c_e_th():
@@ -68,9 +87,9 @@ def c_e_th():
 	et.start()
 	et.join()
 
-def c_w_th():
+def c_w_th(ths):
 	thp=[]
-	for i in range(4):
+	for i in range(ths):
 		t=threading.Thread(target=tfunc,name='tid'+str(os.getpid())+r'/'+str(i))
 		thp.append(t)
 	for a in thp:
@@ -78,21 +97,31 @@ def c_w_th():
 	for b in thp:
 		b.join()
 
-st=time.time()
-wq=queue.Queue(3)
-eq=Queue(1)
-count=10
-i=0
-c=Value('i',5)
-ee=Event()
-ee.set()
-we=Event()
+if __name__=='__main__':
+	st=time.time()
+	procs=2
+	#procs=os.cpu_count()
+	ths=4
+	wqs=4
+	wq=queue.Queue(wqs)
+	eq=Queue(procs)
+	i=0
+	c=Value('i',20)
 
-pe=Process(target=pefunc)
-pw=Process(target=pwfunc)
-pe.start()
-pw.start()
-pe.join()
-pw.join()
+#set event
+	ee=Event()
+	ee.set()
+	we=Event()
+
+#start procs
+	pe=Process(target=pefunc)
+	pe.start()
+	eq_get=0
+	pw=Pool(procs)
+	for i in range(procs):
+		pw.apply_async(pwfunc)
+	pw.close()
+	pe.join()
+	pw.join()
 
 print('use time :',time.time()-st)
