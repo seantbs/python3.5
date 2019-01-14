d4f90a52fe7cbe4a1ca54340761ef3adb4b24b8f
seantbs <seantbs@163.com>
2019-01-15 00:30:46 +0800

    Update pgbar.py

--- py-test/pgbar.py
+++ py-test/pgbar.py
@@ -2,26 +2,19 @@
 
 import sys,time
 
-def bar(mix,max,count,step,n):
+def bar(max,count,step):
 	m='>'
 	p=max/step
-	if count == mix:
-		sys.stdout.write('\r'+'-'*step+'0%')
-		time.sleep(0.1)
-	elif (count+1)%p == 0 and count+1 < max:
-		n+=p
+	n=0
+	if (count+1)/p > 1:
+		n=int((count+1)/p)
 		sys.stdout.write('\r'+m*n+'-'*(step-n)+'%.2f'%(n*p/max*100)+'%')
-		sys.stdout.flush()
-		time.sleep(0.1)
-	elif (count+1) == max:
-		sys.stdout.write('\r'+'>'*step+'100.00%')
-		time.sleep(0.1)
-	return n
+		time.sleep(0.01)
 
 if __name__=='__main__':
 	acc=0
 	c=100
 	for i in range(c):
-		acc=bar(0,c,i,60,acc)
+		bar(c,i,50)
 		
 	print('\nThis is a simple propress bar test.')
