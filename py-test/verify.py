492a17983e437a8249a80a00e7ffffae5bb2711e
seasntbs <seantbs@163.com>
2019-01-17 17:31:55 +0800

    Update verify.py

--- py-test/verify.py
+++ py-test/verify.py
@@ -2,9 +2,8 @@
 
 import sys,io,time,pgbar,threading,queue
 
-def w_y():
-	global allc
-	for i in range(allc):
+def w_y(c):
+	for i in range(c):
 		yield i+1
 		
 def wq_put():
@@ -23,10 +22,11 @@ def wq_put():
 def bar():
 	global n,allc
 	while True:
-		if n == allc:
-			pgbar.bar(allc,n,60,st)
+		if n >= allc:
+			pgbar.bar(allc,n,50,st)
+			e.set()
 			break
-		pgbar.bar(allc,n,60,st)
+		pgbar.bar(allc,n,50,st)
 	print('\nbar is done....')
 
 def wfunc():
@@ -36,24 +36,30 @@ def wfunc():
 		r=[]
 		c=0
 		n+=1
-		print('t2 =',t2)
 		for ii in t1:
-			if  t2 == ii:
+			if  ii == t2:
 				c+=1
 				print('t2 =',t2)
 				r.append(i)
 				r.append(c)
 				t3.append(r)
-				allc+=1
+				if allc%6000 == 0:
+					print('allc =',allc)
 				t1.remove(ii)
+				allc-=1
 		if c == 0:
 			err.append(t2)
 		elif c > 1:
 			err.append(t2)
-	if n+1 <= allc:
+	if allc > 0:
 		e.wait()
 		wq_put()
-	elif n+1 > allc:
+	elif allc <= 0:
+		if not e.is_set():
+			we.clear()
+		elif e.is_set():
+			we.set()
+		we.wait()
 		return
 	#print(threading.current_thread().name, 'is done...')
 
@@ -64,29 +70,31 @@ if __name__=='__main__':
 	f=open('./'+fname,'r+')
 	t1=f.readlines()
 	t3=[]
-	allc=len(t1)
+	task=len(t1)
+	allc=task
 	print(fname,'lines =',allc)
 	err=[]
 	n=0
-	wg=w_y()
+	wg=w_y(task)
 
-	ths=10
+	ths=128
 	wq=queue.Queue(ths)
 	thp=[]
 	e=threading.Event()
 	e.set()
+	we=threading.Event()
 	#et=threading.Thread(target=wfunc,name='tid'+str(i))
 	for i in range(ths):
 		t=threading.Thread(target=wfunc,name='tid'+str(i))
 		thp.append(t)
-	#bar=threading.Thread(target=bar,name='tid-bar')
-	#bar.start()
+	bar=threading.Thread(target=bar,name='tid-bar')
+	bar.start()
 	for a in thp:
 		a.start()
 	for b in thp:
-		b.join(2)
-	#bar.join(2)
-
+		b.join()
+	bar.join()
+	f.close()
 
 	print('\nt1 count:',len(t1),'\nt3 count:',len(t3),'\nerr count:',len(err),'\n')
 	print('use time :',time.time()-st,'s')
