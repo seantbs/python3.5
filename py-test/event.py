c18fbeae69dc56772e332fa55da6208c5b9ec8f0
seantbs <seantbs@163.com>
2019-01-20 10:59:48 +0800

    Update event.py

--- py-test/event.py
+++ py-test/event.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.5
 
 from multiprocessing import Event,Queue,Pool,Process,Value
 import io,os,sys,time,random,threading,queue,pgbar
@@ -18,13 +18,13 @@ def eq_put_y(a,b,c):
 			if a >= b*c:
 				a-=b
 				yield a
-			elif a*c >= b and a < b*c:
+			elif a*c >= b/c and a < b*c:
 				if a > c:
 					a=a-(int(a/c)+a%c)
 					yield a
 				elif a < c:
 					yield 0
-			elif a*c < b:
+			elif a*c < b/c:
 					yield 0
 
 def wq_put_y(a,b):
@@ -43,13 +43,12 @@ def eq_put():
 		task=next(g)
 		if task != 0:
 			eql.append(task)
-			#print('[eq_put]eql :',eql)
+			print('[eq_put]eql :',eql)
 			eq.put(eql)
 		elif task == 0:
 			eql.append(0)
-			#print('[eq_put]eql :',eql)
+			print('[eq_put]eql :',eql)
 			eq.put(eql)
-			#print('[eq_put]eq put value = 0,so eq_put done.')
 			return 0
 	return 1
 
@@ -58,7 +57,6 @@ def eq_get():
 	wqe=[]
 	wqa=None
 	wqb=None
-	we.clear()
 	if not eq.empty():
 		wqe=eq.get()
 		#print('[eq_get]wqe =',wqe)
@@ -81,13 +79,11 @@ def wq_put():
 	except:
 		if not m.empty():
 			m.get()
-		wfunc()
-		
+			wfunc()
+
 	if not wq.full() and x != None:
 		wq.put(x)
-		wfunc()
-	else:
-		wfunc()
+	wfunc()
 
 def wfunc():
 	global n,ptime,pcount,ramf
@@ -101,23 +97,18 @@ def wfunc():
 		pcount+=1
 		time.sleep(r)
 		n+=1
-	bar.value+=n
+
 	if not m.full() and not eq.empty():
 		if not m.full():
 			m.put(threading.current_thread().name)
+			we.clear()
 			eq_get()
 		else:
 			we.wait()
 			wq_put()
 	elif m.empty() and eq.empty():
 		#print('m and eq queue is empty so wfunc done.')
-		if ee.is_set():
-			we.set()
-			return
-		else:
-			we.clear()
-			we.wait()
-			return
+		return
 	else:
 		#print(threading.current_thread().name,'bar value =',bar.value,'ee set :',ee.is_set())
 		we.wait()
@@ -137,7 +128,6 @@ def efunc():
 
 def wfunc_bar():
 	global bartask,st
-	ee.clear()
 	while True:
 		count=bar.value
 		if count < bartask:
@@ -147,18 +137,18 @@ def wfunc_bar():
 			ee.set()
 			break
 		#print('count =',count)
-		time.sleep(0.2)
+		time.sleep(0.01)
 
 def c_e_th():
 	print('event tid',os.getpid(),'is starting...')
 	et=threading.Thread(target=efunc,name='event_tid='+str(os.getpid()))
-	wbar=threading.Thread(target=wfunc_bar,name='wbar_tid='+str(os.getpid()))
+	#wbar=threading.Thread(target=wfunc_bar,name='wbar_tid='+str(os.getpid()))
 	et.start()
-	wbar.start()
+	#wbar.start()
 	et.join()
-	wbar.join()
+	#wbar.join()
 	print('\n[efunc]there is no more task so efunc done.use time:%.2f' % (time.time()-st)+'s')
-	print('*'*60+'\n')
+	print('='*60+'\n')
 	print('waiting for wfunc thread over...')
 
 def c_w_th(ths):
@@ -171,7 +161,6 @@ def c_w_th(ths):
 	for b in thp:
 		b.join()
 	we.set()
-	print('now wq is empty,wait thread over...')
 
 def pefunc():
 	print(os.getpid(),'pefunc is running...')
@@ -180,42 +169,44 @@ def pefunc():
 def pwfunc():
 	global allcount,alltime,reslog,ramf
 	print('[pwfunc]pid =',os.getpid(),'is running...')
-	n=0
 	c_w_th(ths)
 	ramf2=io.StringIO(ramf.getvalue())
 	allcount.value+=pcount
 	alltime.value+=ptime
 	print('\npid ='+str(os.getpid())+' real time: '+str(ptime)+'s\tcounts:'+str(pcount))
 	print('[wfunc]'+str(os.getpid())+' wfunc is done use time:%.2f' % (time.time()-st)+'s')
-	sys.stdout.flush()
-	while True:
-		ram2res=ramf2.readline()
-		n+=1
-		if ram2res == '':
+
+	ram2res=[]
+	ram2res=ramf2.readlines()
+	for i in ram2res:
+		if i == '':
 			break
 		try:
-			reslog.write(ram2res)
+			reslog.write(i)
 		except:
 			print('test.log write erroe at line:',ram2res)
 			continue
-		reslog.flush()
+	reslog.flush()
 
 if __name__=='__main__':
 	st=time.time()
+
 #main public var
+	fname='result.log'
 	try:
-		os.remove('./test.log')
+		os.remove('./'+fname)
 	except:
 		pass
-	os.path.exists("./test.log")
-	reslog=open('./test.log','a')
+	os.path.exists('./'+fname)
+	reslog=open('./'+fname,'a+')
 	ramf=io.StringIO()
-	procs=8
-	ths=512
-	wqs=ths*2
+
+	procs=10
+	ths=2048
+	wqs=int(ths/procs)
 	#procs=os.cpu_count()
 	eq=Queue(procs)
-	task=655360
+	task=132000
 	bartask=task
 	alltime=Value('i',0)
 	allcount=Value('i',0)
