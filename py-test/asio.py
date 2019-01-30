fd23fb6e771887840f149133b78bfa09d6d07da2
seasntbs <seantbs@163.com>
2019-01-30 17:32:29 +0800

    Update asio.py

--- py-test/asio.py
+++ py-test/asio.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3.5
 
-import asyncio,time
+import asyncio,time,queue,random
 
-def consumer():
+'''def consumer():
 	r = ''
 	while True:
 		n = yield r
@@ -19,13 +19,64 @@ def produce(c):
 		print('[PRODUCER] Producing %s...' % n)
 		r = c.send(n)
 		print('[PRODUCER] Consumer return: %s' % r)
-	c.close()
+	c.close()'''
+
+async def compute(x, y):
+	print("Compute %s + %s ..." % (x, y))
+	await asyncio.sleep(1.0)
+	return x + y
+
+async def print_sum(x, y):
+	result = await compute(x, y)
+	print("%s + %s = %s" % (x, y, result))
+
+###########################################################
+'''async def wq_put():
+	global i,st
+	while True:
+		i+=1
+		print('[wq_put]i =',i,'<->',time.time()-st)
+		wq.put(i)
+		await asyncio.sleep(1)
+		if i < 5:
+			continue
+		else:
+			return
+
+async def wq_get():
+	global text
+	print('wq get is starting...')
+	while not wq.empty():
+		text=''
+		try:
+			x=str(wq.get())
+		except:
+			await wq_put()
+		print('[wq_get]x =',x)
+		r=random.randint(2,8)
+		for i in range(r):
+			text+='a'
+			await asyncio.sleep(1)
+		print('[wq_get]text : '+x+','+text)'''
 
 if __name__ == '__main__':
 	st=time.time()
 	
-	c = consumer()
-	produce(c)
+	'''i=0
+	text=''
+	wq=queue.Queue(4)
 	
-	print('use time: %.2f ' % (time.time()-st)+'s')
+	#coroutine test
+	c = consumer()
+	produce(c)'''
+	
+	loop = asyncio.get_event_loop()
+	loop.run_until_complete(print_sum(1, 2))
+	loop.close()
 
+	'''loop = asyncio.get_event_loop()
+	astks = [wq_get(),wq_put()]
+	loop.run_until_complete(asyncio.wait(astks))
+	loop.close()'''
+	
+	print('use time: %.3f ' % (time.time()-st)+'s')
