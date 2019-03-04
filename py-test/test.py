7c112df9b661b35f3fc202422dd5396e852b6387
seasntbs <seantbs@163.com>
2019-03-04 15:56:08 +0800

    Update test.py

--- py-test/test.py
+++ py-test/test.py
@@ -1,5 +1,7 @@
-import asyncio,sys,time
+import asyncio,sys,time,queue,random,os
+import threading,multiprocessing
 
+#test future
 async def slow_operation(future):
 	print('[fun]Future is done!')
 	await wait_time(1)
@@ -13,19 +15,79 @@ async def wait_time(t):
 def got_result(future):
     print(future.result())
     loop.stop()
+####################################################
+
+#test loop
+async def work():
+	global ct,cw,res_cache,workers
+	while True:
+		std = ''
+		text = ''
+		x = None
+		r = random.randint(2,6)
+		if not wq.empty():
+			x = wq.get()
+			ct+=r
+			cw+=1
+			print('[work_%s]work %s is running...' % (x,x))
+			for i in range(r):
+				text+='a'
+				await asyncio.sleep(1)
+			std=str(x)+'\t'+text+'\n'
+			print('[work_%s]x = %s,r = %s,std = %s' % (x,x,r,std))
+			res_cache.append(std)
+			if cw%(workers) < workers/100:
+				await res_save()
+		else:
+			while len(res_cache):
+				await res_save()
+			return
+
+async def res_save():
+	global workers,res_cache
+	v=''
+	for i in range(workers):
+		try:
+			v+=res_cache.pop()
+		except:
+			continue
+	try:
+		reslog.write(v)
+	except:
+		errlist.append(v)
+	reslog.flush()
+	return
 
 if __name__=='__main__':
 	st=time.time()
-	loop = asyncio.get_event_loop()
-	future = asyncio.Future()
-	asyncio.ensure_future(slow_operation(future))
-	loop.run_until_complete(future)
-	'''print(future.result())
-	loop.close()'''
-	future.add_done_callback(got_result)
+	
+	fname='./test.log'
 	try:
-		loop.run_forever()
-	finally:
-		loop.close()
-
+		os.remove(fname)
+	except:
+		pass
+	os.path.exists(fname)
+	reslog=open(fname,'a')
+	
+	ct=0
+	cw=0
+	res_cache=[]
+	workers=20000
+	counts=6600000
+	
+	wq=queue.Queue()
+	for i in range(counts):
+		wq.put(i+1)
+	
+	loop = asyncio.get_event_loop()
+	coros=[]
+	for i in range(workers):
+		coros.append(work())
+	fs=asyncio.gather(*coros)
+	loop.run_until_complete(fs)
+	loop.close()
+	print('no work to do...',wq.qsize())
+	reslog.close()
+	
+	print('real time : %s\tcounts : %s' % (ct,cw))
 	print('use time:%.4f'%(time.time()-st))
